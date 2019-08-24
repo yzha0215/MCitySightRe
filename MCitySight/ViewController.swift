6662056d829a18ff13aa3bcdfa33c80fea15fbd4
Yuqi Zhang <33114858+yzha0215@users.noreply.github.com>
2019-08-24 13:46:49 +1000

    Initial Commit

---
+++ MCitySight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCitySight
+//
+//  Created by Yuqi Zhang on 24/8/19.
+//  Copyright © 2019 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
